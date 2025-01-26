1200d1ea5ebe448d5f2a500227263847f288ee60
Sandić Nenad <sandicnenad81@gmail.com>
2025-01-26 13:48:04 +0100

    s0103: UnionFind - Quick Find

---
+++ Algorithms Princeton University by Robert Sedgewick/MyDataStructure/UnionFind.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_Princeton_University_by_Robert_Sedgewick.MyDataStructure
+{
+    public class UnionFind
+    {
+        private readonly int _n;
+        private int[] _unionFindArray;
+        public UnionFind()
+        {
+            _n = 0;
+            _unionFindArray = [];
+        }
+        public UnionFind( int n)
+        {
+            _n = n;
+            _unionFindArray = new int[n];
+            for (int i = 0; i < _unionFindArray.Length; i++)
+            {
+                _unionFindArray[i] = i;
+            }
+        }
+
+        public bool IsConnected(int p, int q)
+        {
+            if (_unionFindArray is null)
+                return false;
+
+            return _unionFindArray[p] == _unionFindArray[q];
+        }
+
+        public void Union(int p, int q)
+        {
+            int elementPid = _unionFindArray[p];
+            int elementQid = _unionFindArray[q];
+
+            for (int i = 0; i< _unionFindArray.Length; i++)
+            {
+                if ( _unionFindArray [i] == elementPid)
+                    _unionFindArray[i] = elementQid;
+            }
+        }
+
+        public void ShowArray()
+        {
+            string emptyArray = "Union Find array not created, array is empty!";
+            string notEmptyArray = "Elements of our Union Find class are:";
+            string initialMessage = _unionFindArray == null ? notEmptyArray : emptyArray;
+            Console.WriteLine(initialMessage);
+
+            for (int i = 0;i < _unionFindArray?.Length;i++)
+            {
+                Console.WriteLine(_unionFindArray[i]);
+            }
+        }
+    }
+}
--- Algorithms Princeton University by Robert Sedgewick/Program.cs
+++ Algorithms Princeton University by Robert Sedgewick/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Algorithms_Princeton_University_by_Robert_Sedgewick.MyDataStructure;
+
 Console.WriteLine("Hello, World!");
+
+//new UnionFind(9);
+
+UnionFind.ShowArray();
+
+Console.ReadLine();
